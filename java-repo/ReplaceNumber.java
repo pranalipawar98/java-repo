c1b8af20caff3cb8c7763586b8b678a9d77b94f7
Pranali Pawar <pranalipawar98@gmail.com>
2019-01-06 20:47:27 +0530

    Program to print number between 1 to n replacing multiples of 3, multiples of 5 and multiples of 15 by some string

---
+++ java-repo/ReplaceNumber.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReplaceNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter number of element : ");
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt();
+		System.out.println(fizzBuzz(num));
+		
+		
+	}
+	public static List<String> fizzBuzz(final int n)
+	{
+		final List<String> toReturn = new ArrayList<>();
+		for(int i = 1; i<=n ; i++)
+		{
+			if(i % 15 == 0)
+			{
+				toReturn.add("FizzBuzz");
+			}
+			else if(i % 3 == 0)
+			{
+				toReturn.add("Fizz");
+			}
+			else if(i % 5 == 0)
+			{
+				toReturn.add("Buzz");
+			}
+			else
+			{
+				toReturn.add(Integer.toString(i));
+			}
+			
+		}
+		return toReturn;
+	}
+}
